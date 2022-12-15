62fcc198b663267c4aa079eba1d8e7e496c0cd63
Bodomrus70@mail.ru <Bodomrus70@mail.ru>
2022-12-15 16:30:08 +0700

    Добавьте файлы проекта.

---
+++ Module_Itog_8.6.1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+
+namespace Module_8_6_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Укажите путь к папки:");
+            string path = Console.ReadLine();
+            DirectoryInfo di = new DirectoryInfo(path);
+            if (di.Exists)
+            {
+                deleteFolder(path);
+            }
+            else
+            {
+                Console.WriteLine("Папки не существует");
+            }
+
+        }
+        private static void deleteFolder(string folder)
+        {
+            try
+            {
+                DirectoryInfo di = new DirectoryInfo(folder);
+                DirectoryInfo[] diA = di.GetDirectories();
+                FileInfo[] fi = di.GetFiles();
+                foreach (FileInfo f in fi)
+                {
+                    if (DateTime.Now - f.LastAccessTime > TimeSpan.FromMinutes(30) && f.Exists)
+                    {
+                        f.Delete();
+                    }
+                }
+                foreach (DirectoryInfo df in diA)
+                {
+                    if (df.Exists)
+                    {
+                        deleteFolder(df.FullName);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Папки не существует");
+                    }
+
+                }
+                if (di.GetDirectories().Length == 0 && di.GetFiles().Length == 0) di.Delete();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка: " + ex.Message);
+            }
+        }
+    }
+}
